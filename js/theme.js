3b791a5e996ea48b69096e7d1ad19b7e8a0f35c2
magiccia <magiccia@gmail.com>
2016-01-09 22:32:14 -0500

    starting the website

--- js/theme.js
+++ js/theme.js
@@ -59,14 +59,6 @@ function trackScroll() {
 
 }
 
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
 
 $(window).scroll(debounce(trackScroll, 250));
 
